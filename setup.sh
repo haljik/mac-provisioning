797003a1e704caf5f6f9ae476e5e482a66ce745e
haljik <haljik@haljik-no-MacBook-Air.local>
2015-10-23 02:17:42 +0900

    initial commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install git
+brew install vim
+brew install zsh
+
+brew install caskroom/cask/brew-cask
+
+brew cask install macvim
+brew cask install google-japanese-ime
+brew cask install karabiner
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install macvim
+brew cask install slack
+brew cask install dockertoolbox
+brew cask install java
+brew cask install intellij-idea
+brew install maven
+brew install gradle
+
+brew cask install alfred
+
